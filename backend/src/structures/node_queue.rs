1491b6083fd6d8d6b9adb70063fb28b52d3acc75
Magnus Lutro Allison <magnulal@stud.ntnu.no>
2023-05-16 09:47:22 +0200

    Changed NodeQueue to use queues::*

--- backend/src/structures/node_queue.rs
+++ backend/src/structures/node_queue.rs
@@ -1,28 +1,52 @@
-use priority_queue::PriorityQueue;
 use crate::structures::node::Node;
+use queues::*;
+use std::collections::HashSet;
 
 //Defines a node queue struct
 struct NodeQueue {
-    queue: PriorityQueue<Node>,
+    queue: Queue::<Node>::new(),
 }
 
 //Implementation of the node queue struct
 impl NodeQueue {
     pub fn new() -> NodeQueue {
         NodeQueue {
-            queue: PriorityQueue::new(),
+        queue: Queue::<Node>::new(),
         }
     }
 
-    pub fn push(&mut self, node: Node) {
-        self.queue.push(node, node.weight);
+    //Adds a node to the queue
+    pub fn add(&mut self, node: Node) {
+        self.queue.add(node);
     }
 
+    //Removes a node from the queue
+    pub fn remove(&mut self) -> Option<Node> {
+        self.queue.remove()
+    }
+
+    //Removes the first node from the queue
+    pub fn remove_first(&mut self) -> Option<Node> {
+        self.queue.remove_first()
+    }
+
+    //Pops the last node from the queue
     pub fn pop(&mut self) -> Option<Node> {
         self.queue.pop()
     }
 
+    //Peek the first node from the queue
+    pub fn peek(&self) -> Option<&Node> {
+        self.queue.peek()
+    }
+
+    //Returns true if the queue is empty
     pub fn is_empty(&self) -> bool {
         self.queue.is_empty()
+    }
+
+    //Returns the length of the queue
+    pub fn len(&self) -> usize {
+        self.queue.len()
     }
 }
